90a32a3bf124734795ce0035367b0bc0916433ce
DharmeshV05 <dharmeshbm1@gmail.com>
2025-03-28 16:27:46 +0530

    Update CORS to allow Vercel frontend origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://voucher-form-frontend-nu.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
